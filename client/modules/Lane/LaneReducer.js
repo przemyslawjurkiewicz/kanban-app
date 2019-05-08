4d079c9ace865bd20bef5b65d41102fe7ae11175
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2019-05-08 17:09:13 +0200

    Add lane reducer

--- client/modules/Lane/LaneReducer.js
+++ client/modules/Lane/LaneReducer.js
@@ -1,14 +1,23 @@
 // Import Actions
-import {  } from './LaneActions';
+import { CREATE_LANE, UPDATE_LANE, DELETE_LANE } from './LaneActions';
 
 // Initial State
-const initialState = {};
+const initialState = [];
 
-const LaneReducer = (state = initialState, action) => {
+export default function lanes(state = initialState, action) {
   switch (action.type) {
+    case CREATE_LANE:
+      return [...state, action.lane];
+    case UPDATE_LANE:
+      return state.map(lane => {
+        if (lane.id === action.id) {
+          return Object.assign({}, lane, action.lane);
+        }
+        return lane;
+      });
+    case DELETE_LANE:
+      return state.filter(lane => lane.id !== action.laneId);
     default:
       return state;
   }
-};
-
-export default LaneReducer;
+}
